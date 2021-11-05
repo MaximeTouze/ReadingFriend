582d02e97cdeb5b7092641861c6205dac853a604
s171369 <s171369@ic2s210-01.local>
2021-11-05 14:41:10 +0100

    ajout du fichier BERT_QnA.py + ajout d'un test de bert sur server.py

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 # flask_app/server.py​
 from flask import Flask, request, jsonify, render_template, session, url_for, redirect
 from flask_dropzone import Dropzone
+from BERT_QnA import getAnswerBert
 
 import settings
 
@@ -9,6 +10,12 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/")
+def test():
+    context = "BERT-large is really big ... it has 24 layers and an embedding size of 1024 for a total of 340M parameters! Altogether it is 1.34GB, so expect it to take a couple of minutes to download."
+    question = "How many parameters does BERT-large have ?"
+    print(getAnswerBert(question, context))
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True, port=PORT)
