f6e15ba2a2b5de40701a0581732e35d549b0b466
HugoRig <hugo.riguidel@gmail.com>
2021-11-22 10:04:29 +0100

    modification des packages nécessaires

--- server.py
+++ server.py
@@ -11,16 +11,16 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/")
-def test():
-    context = "BERT-large is really big ... it has 24 layers and an embedding size of 1024 for a total of 340M parameters! Altogether it is 1.34GB, so expect it to take a couple of minutes to download."
-    question = "How many parameters does BERT-large have ?"
+#@app.route("/")
+#def test():
+#    context = "BERT-large is really big ... it has 24 layers and an embedding size of 1024 for a total of 340M parameters! Altogether it is 1.34GB, so expect it to take a couple of minutes to download."
+#    question = "How many parameters does BERT-large have ?"
 
-    prediction = getAnswerBert(question, context)
+#    prediction = getAnswerBert(question, context)
 
-    print(prediction)
+#    print(prediction)
 
-    return prediction
+#    return prediction
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True, port=PORT)
