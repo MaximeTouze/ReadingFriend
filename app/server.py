bb0ce0a5a2999319eaf4442427786518288d9671
s171369 <s171369@ic2s210-01.local>
2021-11-24 14:26:50 +0100

    ajout sélection des livres automatique

--- app/server.py
+++ app/server.py
@@ -1,27 +1,57 @@
 # flask_app/server.py​
 from flask import Flask, request, jsonify, render_template, session, url_for, redirect
 from flask_dropzone import Dropzone
-from BERT_QnA import getAnswerBert
+#from BERT_QnA import getAnswerBert
 
 import settings
+import glob
+import os
+import sys
+import json
 
 app = Flask(__name__)
 
+path = "static/books"
+
+# Renders the page before a valid form submition
 @app.route("/")
 def index():
-    return render_template("index.html")
+    # Sends file list and text list
+    return render_template("index.html", book_list=json.dumps(get_books_files()), book_text_list=json.dumps(get_books_texts()))
 
+# Renders the page when a valid form is submitted
 @app.route("/submit", methods=['POST'])
 def get_answer():
-    #if request.method == 'POST':
+    # Gets the text and question from the client
     context = request.form['text']
     question = request.form['question']
 
-    prediction = getAnswerBert(question, context)
+    # Finds the question's answer in the text (using a BERT model)
+    prediction = "test" #getAnswerBert(question, context)
 
-    return render_template("index.html", answer=prediction)
-    #else:
-    #    return render_template("index.html")
+    # Sends question's answer, file list and text list
+    return render_template("index.html", answer=prediction, book_list=json.dumps(get_books_files()), book_text_list=json.dumps(get_books_texts()))
+
+# Returns a list containing all file names representing a book stored on the website
+def get_books_files():
+    file_list = []
+
+    for file in os.listdir(path):
+        if file.endswith(".txt"):
+            file_list.append(file)
+
+    return file_list
+
+# Returns a list containing all texts from books stored on the website
+def get_books_texts():
+    text_list = []
+
+    for file in os.listdir(path):
+        if file.endswith(".txt"):
+            with open(path + "/" + file, 'r') as filename:
+                text_list.append(filename.read())
+
+    return text_list
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True, port=PORT)
