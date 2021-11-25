2c43d082c102dbc5bfb872cb3924141f7e92f67e
s171369 <s171369@ic2s210-01.local>
2021-11-25 14:15:42 +0100

    suppression books.php

--- app/books.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $path = "books/"
-  $books = json_encode(glob($path + "*.txt"));
-  //header('Content-Type: application/json');
-  //header('Content-Length: '.strlen($a));
-  //header('Access-Control-Allow-Origin: *'); want to acces from crossdomain???
-  echo $books;
-?>
