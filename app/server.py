7b3b6ff12e6354bde68e283c6abe43a9b030d48c
LogicSystem <hugo.riguidel@gmail.com>
2022-01-07 10:09:08 +0100

    tts

--- app/server.py
+++ app/server.py
@@ -70,7 +70,7 @@ def delete_audio_files():
     while(os.path.isfile("./tts/" + i + "_20180505_deepvoice3_checkpoint_step000640000.wav")):
         os.system("rm ./tts/" + i + "_20180505_deepvoice3_checkpoint_step000640000.wav")
         os.system("rm ./tts/" + i + "_20180505_deepvoice3_checkpoint_step000640000_alignment.png")
-        i++
+        i += 1
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True, port=PORT)
